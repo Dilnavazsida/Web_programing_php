1cf475a8e7a68b3dae2c5d9f8ad2b7d5233e057e
Dilnavazsida <navazdil07@gmail.com>
2024-09-05 21:36:06 +0530

    Add files via upload

---
+++ clock.php
@@ -0,0 +1,65 @@
+
+<html>
+    <head>
+        <link rel="stylesheet" href="clock.css">
+</head>
+<body>
+<body>
+       
+<div class="first-div">
+    <table class="first-table" width="100%">
+        <tr class="date">
+            <th colspan="2">DATE</th>
+            <th colspan="4">MONTH</th>
+            <th colspan="3">YEAR</th>
+        </tr>
+
+        <tr class="first-num">
+            <td colspan="2">05</td>
+            <td colspan="4">08</td>
+            <td colspan="3">24</td>
+        </tr>
+
+        <tr class="hour">
+            <th colspan="2">HOUR</th>
+            <th colspan="4">MINUTE</th>
+            <th colspan="3">SECOND</th>
+        </tr>
+        <tr class="second-num">
+            <td colspan="2">12</td>
+            <td>:</td>
+            <td colspan="2">45</td>
+            <td>:</td>
+            <td colspan="2">52</td>
+        </tr><br>
+        
+        <tr class="days">
+            <td><input type="radio"><label for="">SUN</label></td>
+            <td><input type="radio"><label for="">MON</label></td>
+            <td><input type="radio"><label for="">TUS</label></td>
+            <td><input type="radio"><label for="">WED</label></td>
+            <td><input type="radio"><label for="">THU</label></td>
+            <td><input type="radio"><label for="">FRI</label></td>
+            <td><input type="radio"><label for="">SAT</label></td>
+        </tr>
+    </table>
+
+    <div class="ampm">
+    
+        <div id="am" class="color_am"></div>
+        <p id="AM">AM</p>
+        <div id="pm" class="color_pm"> </div>
+        <p id="AM">PM</p>
+        <p class="days-of-week" style="color:green"></p>  
+    </div>
+    <div class="dilnavaz"> Dilnavaz A Sida</div>
+    
+</div>       
+</body>
+    </html>
+
+    <?php
+
+?>
+
+
